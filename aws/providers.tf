39f474c3a60af421360773e0235378351c2fce60
taleldayekh <dev@talel.io>
2023-03-23 19:56:10 +0100

    Move Docker to AWS root providers

--- aws/providers.tf
+++ aws/providers.tf
@@ -3,10 +3,26 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
   }
 }
 
+module "ecr" {
+  source = "./ecr"
+}
+
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = var.aws_region
   shared_credentials_file = "~/.aws/credentials"
+}
+
+provider "docker" {
+  registry_auth {
+    address  = "${module.ecr.aws_identity.account_id}.dkr.ect.${var.aws_region}.amazonaws.com"
+    username = module.ecr.aws_token.user_name
+    password = module.ecr.aws_token.password
+  }
 }
