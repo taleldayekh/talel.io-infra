9a36288a20d691bd836ba76320f8b5a3bf2c2d1c
taleldayekh <dev@talel.io>
2023-03-24 08:59:25 +0100

    Remove Docker from root providers

--- aws/providers.tf
+++ aws/providers.tf
@@ -3,26 +3,10 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
   }
-}
-
-module "ecr" {
-  source = "./ecr"
 }
 
 provider "aws" {
   region                  = var.aws_region
   shared_credentials_file = "~/.aws/credentials"
-}
-
-provider "docker" {
-  registry_auth {
-    address  = "${module.ecr.aws_identity.account_id}.dkr.ect.${var.aws_region}.amazonaws.com"
-    username = module.ecr.aws_token.user_name
-    password = module.ecr.aws_token.password
-  }
 }
