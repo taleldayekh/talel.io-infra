7c61969fccb6a85606d7b1a1235968e6072dafbb
taleldayekh <dev@talel.io>
2023-03-29 21:28:47 +0200

    Adapt IAM module

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -1,29 +1,18 @@
-data "aws_iam_policy_document" "ec2" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ec2" {
+resource "aws_iam_role" "ec2_role" {
   name               = "talelio-ec2-role"
-  assume_role_policy = data.aws_iam_policy_document.ec2.json
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
 
   tags = {
     Name = "talelio-ec2-role"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ec2" {
-  role       = aws_iam_role.ec2.name
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_instance_profile" "ec2" {
-  name = "talelio-instance-profile"
-  role = aws_iam_role.ec2.name
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "talelio-ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
 }
--- aws/iam/outputs.tf
+++ aws/iam/outputs.tf
@@ -1,3 +1,3 @@
-output "instance_profile_name" {
-    value = aws_iam_instance_profile.ec2.name
+output "ec2_instance_profile" {
+  value = aws_iam_instance_profile.ec2_instance_profile.name
 }
