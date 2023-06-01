49f6edd1eb0d51cab13317a683f314ee4bdc07e0
taleldayekh <dev@talel.io>
2023-06-01 08:09:09 +0200

    Prepare ECR module for refactoring

--- aws/ecr/data.tf
+++
@@ -1,3 +0,0 @@
-data "aws_caller_identity" "aws_identity" {}
-
-data "aws_ecr_authorization_token" "aws_token" {}
--- aws/ecr/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "talelio_postgresql_image_uri" {
-  value = docker_image.talelio_postgresql.name
-}
