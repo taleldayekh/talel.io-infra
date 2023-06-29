285ac002d8234147fc4f772f737177a6df489f4a
taleldayekh <dev@talel.io>
2023-06-29 22:00:09 +0200

    Create postgres Docker image

--- aws/ecr/main.tf
+++ aws/ecr/main.tf
@@ -12,7 +12,7 @@ resource "aws_ecr_lifecycle_policy" "talelio_repository_lifecycle" {
         "rules": [
             {
                 "rulePriority": 1,
-                "description": "Keep latest image only",
+                "description": "Keep latest two images only",
                 "selection": {
                     "tagStatus": "any",
                     "countType": "imageCountMoreThan",
@@ -27,4 +27,25 @@ resource "aws_ecr_lifecycle_policy" "talelio_repository_lifecycle" {
     EOF
 }
 
-# resource "docker_image" "talelio_postgresql" {}
+resource "docker_image" "talelio_postgresql" {
+  # TODO: Understand image naming
+  name = "talelio-postgresql"
+
+  build {
+    context    = "${path.root}/dockerfiles/postgres"
+    dockerfile = "Dockerfile"
+    no_cache   = true
+    platform   = "linux/amd64"
+
+    build_args = {
+      POSTGRES_USER              = var.postgres_user
+      POSTGRES_PASSWORD          = var.postgres_password
+      POSTGRES_DB                = var.postgres_db
+      AWS_ACCESS_KEY_ID          = var.aws_access_key_id
+      AWS_SECRET_ACCESS_KEY      = var.aws_secret_access_key
+      S3_BACKUPS_BUCKET          = var.s3_backups_bucket
+      S3_POSTGRES_BACKUPS_PREFIX = var.s3_postgres_backups_prefix
+      RESTORE_DB_SCRIPT_PATH     = "${path.root}/restore_postgres_backup.sh"
+    }
+  }
+}
--- aws/ecr/variables.tf
+++ aws/ecr/variables.tf
@@ -7,3 +7,31 @@ variable "talelio_repositories" {
     "talelio-nginx"
   ]
 }
+
+variable "postgres_user" {
+  type = string
+}
+
+variable "postgres_password" {
+  type = string
+}
+
+variable "postgres_db" {
+  type = string
+}
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
+
+variable "s3_backups_bucket" {
+  type = string
+}
+
+variable "s3_postgres_backups_prefix" {
+  type = string
+}
