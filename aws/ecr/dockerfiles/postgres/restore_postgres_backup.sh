57fb88ad243066c6864bc3b0d60574c5397a4c32
taleldayekh <dev@talel.io>
2023-06-29 19:54:48 +0200

    Update db restore script

---
+++ aws/ecr/dockerfiles/postgres/restore_postgres_backup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+latest_db_backup_filename=$( \
+aws s3api list-objects \
+--bucket $S3_BACKUPS_BUCKET \
+--prefix $S3_POSTGRES_BACKUPS_PREFIX \
+--query 'reverse(sort_by(Contents, &LastModified))[0].Key' \
+--output text \
+| tail -n1 \
+| awk -F/ '{print $NF}')
+
+if [ -z '$latest_db_backup_filename']
+then
+    aws s3 cp s3://$S3_BACKUPS_BUCKET/$S3_POSTGRES_BACKUPS_PREFIX/$latest_db_backup_filename ~
+    psql -U $POSTGRES_USER -d $POSTGRES_DB < ~/$latest_db_backup_filename
+else
+    echo 'Database backup file could not be found in S3'
+fi
