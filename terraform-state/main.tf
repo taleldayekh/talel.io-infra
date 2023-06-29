32d5665ab398db1817e7596fe6d5c1dda64c3035
taleldayekh <dev@talel.io>
2023-06-29 19:15:54 +0200

    Add resource for creating db backup directory

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -11,10 +11,18 @@ provider "aws" {
   shared_credentials_files = ["~/.aws/credentials"]
 }
 
+# TODO: Parameterize bucket name and key for the below resources
+
 resource "aws_s3_bucket" "talelio_backups" {
   bucket = "talelio-backups"
 
   lifecycle {
     prevent_destroy = true
   }
+}
+
+resource "aws_s3_object" "postgres_db_backups" {
+  bucket       = "talelio-backups"
+  key          = "postgres-db-backups"
+  content_type = "application/x-directory"
 }
