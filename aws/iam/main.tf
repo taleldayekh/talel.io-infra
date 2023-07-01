6d5d7e15ebc4ab15d16b1a33ee3bb87c8a164699
taleldayekh <dev@talel.io>
2023-07-01 23:15:41 +0200

    EC2 IAM role

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -6,13 +6,3 @@ resource "aws_iam_role" "ec2_role" {
     Name = "talelio-ec2-role"
   }
 }
-
-resource "aws_iam_role_policy_attachment" "policy_attachment" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "talelio-ec2-instance-profile"
-  role = aws_iam_role.ec2_role.name
-}
